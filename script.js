2d0cbccc18b4cd8937ba6496f488bf09432d0ae7
ilinca01101001 <146210425+ilinca01101001@users.noreply.github.com>
2024-02-08 17:01:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function showPage(pageId) {
+    var pages = document.querySelectorAll('.content');
+    pages.forEach(function(page) {
+        if (page.id === pageId) {
+            page.style.display = 'block';
+        } else {
+            page.style.display = 'none';
+        }
+      toggleMenu();
+    });
+}
+
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('menu-show');
+}
